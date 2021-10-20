7f8de24b3a99547d3e29d0ddb9f094dfc14acdd9
Jo Smets <jo.smets@studiohyperdrive.be>
2021-10-20 18:14:14 +0200

    chore(statuses): expose hook

--- public/lib/api/hooks.ts
+++ public/lib/api/hooks.ts
@@ -1,6 +1,7 @@
-import { useWorkflow } from '../hooks';
+import { usePaginatedWorkflowStatuses, useWorkflow } from '../hooks';
 import { WorkflowsModuleAPI } from '../workflows.types';
 
 export const hooks: WorkflowsModuleAPI['hooks'] = {
 	useWorkflow,
+	usePaginatedWorkflowStatuses,
 };
