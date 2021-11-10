fb57d02ab00c23a464dbda9482a77806fcbefe3d
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2021-11-10 15:43:18 +0100

    feat(ctTab): show workflow when no assign rights but disable actions

--- public/lib/components/ContentTypeDetailTab/ContentTypeDetailTab.tsx
+++ public/lib/components/ContentTypeDetailTab/ContentTypeDetailTab.tsx
@@ -106,6 +106,13 @@ const ContentTypeDetailTab: FC<ExternalTabProps> = ({
 			)
 		);
 	}, [initialWorkflowStatuses, newStatusSystemName, newWorkflowStatuses]);
+	const canAssign = useMemo(() => {
+		return rolesRightsConnector.api.helpers.checkSecurityRights(mySecurityrights, [
+			rolesRightsConnector.securityRights.assignWorkflow,
+		]);
+	}, [mySecurityrights]);
+
+	console.log('canAssign', canAssign);
 
 	useEffect(() => {
 		rolesRightsConnector.api.store.roles.service.getSiteRoles(siteId);
@@ -295,6 +302,7 @@ const ContentTypeDetailTab: FC<ExternalTabProps> = ({
 											label="Workflow"
 											placeholder="Selecteer een workflow"
 											options={workflowOptions}
+											disabled={!canAssign}
 										/>
 									</div>
 								</div>
@@ -306,27 +314,29 @@ const ContentTypeDetailTab: FC<ExternalTabProps> = ({
 									</div>
 								</div>
 
-								<ActionBar className="o-action-bar--fixed" isOpen>
-									<ActionBarContentSection>
-										<div className="u-wrapper row end-xs">
-											<Button
-												className="u-margin-right-xs"
-												onClick={onCancel}
-												negative
-											>
-												{t(CORE_TRANSLATIONS.BUTTON_CANCEL)}
-											</Button>
-											<Button
-												disabled={disableSave}
-												onClick={submitForm}
-												type="success"
-												htmlType="submit"
-											>
-												{t(CORE_TRANSLATIONS.BUTTON_SAVE)}
-											</Button>
-										</div>
-									</ActionBarContentSection>
-								</ActionBar>
+								{canAssign && (
+									<ActionBar className="o-action-bar--fixed" isOpen>
+										<ActionBarContentSection>
+											<div className="u-wrapper row end-xs">
+												<Button
+													className="u-margin-right-xs"
+													onClick={onCancel}
+													negative
+												>
+													{t(CORE_TRANSLATIONS.BUTTON_CANCEL)}
+												</Button>
+												<Button
+													disabled={disableSave}
+													onClick={submitForm}
+													type="success"
+													htmlType="submit"
+												>
+													{t(CORE_TRANSLATIONS.BUTTON_SAVE)}
+												</Button>
+											</div>
+										</ActionBarContentSection>
+									</ActionBar>
+								)}
 								<ControlledModal
 									show={showConfirmModal}
 									onClose={onPromptCancel}
